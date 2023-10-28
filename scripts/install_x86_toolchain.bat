8f562e47784b8094f70063ab5ccba43659aa2ee0
AmosLHM <heyman0510@hotmail.com>
2023-10-28 14:04:49 -0600

    Added switches and notes on install_x86_toolchain.bat

--- scripts/install_x86_toolchain.bat
+++ scripts/install_x86_toolchain.bat
@@ -11,9 +11,11 @@ REM Change the path to the directory where you want to install the script
 set INSTALL_PATH=C:\x86_toolchain
 
 REM Copy the script to the installation directory
-copy x86_toolchain.sh %INSTALL_PATH%
+REM "/Y" switch is included to suppress prompting to confirm overwriting the destination file
+copy /Y "x86_toolchain.sh" "%INSTALL_PATH%"
 
 REM Add the installation directory to the system PATH
-setx PATH "%PATH%;%INSTALL_PATH%"
+REM "/M" switch is included to set the system-wide PATH variable
+setx PATH "%PATH%;%INSTALL_PATH%" /M
 
 echo Installation Completed.
