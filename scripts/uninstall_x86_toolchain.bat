83e8325afade384d5ddd4a48dbe1a7aababa57ab
AmosLHM <heyman0510@hotmail.com>
2023-10-28 14:16:27 -0600

    Added switches and notes on uninstall_x86_toolchain.bat

--- scripts/uninstall_x86_toolchain.bat
+++ scripts/uninstall_x86_toolchain.bat
@@ -11,9 +11,11 @@ REM Change the path to the installation directory
 set INSTALL_PATH=C:\x86_toolchain
 
 REM Remove the script from the installation directory
-del %INSTALL_PATH%\x86_toolchain.sh
+REM "/f" switch is included to force delete the file without asking for confirmation
+del /f "%INSTALL_PATH%\x86_toolchain.sh"
 
 REM Remove the installation directory from the system PATH
-setx PATH "%PATH:;C:\x86_toolchain=%"
+REM "/M" switch is included to ensure that the change affects the system-wide PATH variable
+setx PATH "%PATH:;C:\x86_toolchain=%" /M
 
 echo Uninstallation Completed.
